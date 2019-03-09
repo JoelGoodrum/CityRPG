1e9460a7786b9c43e33b3ee39efff0d948a06c6e
Joel <joelgoodrum@hotmail.com>
2019-03-09 13:22:48 -0500

    Downtown, E.Village, GovDistrict, LowCastles, N.Beach, School District, Uptown classes created

---
+++ EastVillage.java
@@ -0,0 +1,22 @@
+//east village map
+/*
+"                       ##School District##        \n"
+"          ______________     _______________\n"
+"          |            |     |             | "
+"          | E.Village  |     | Senior      |               \n"
+"          |  Apartments|     |  Apartments |   "
+"		   |____________|||||||_____________|						"
+"                       =     =
+"##Midtown  --  --  --  =     = --  --  --            ##High Way## "
+ "          ___________ =     =______          \n"+
+"          |            |||||||      |              \n"
+"          | May        |     | Post \____      \n"
+"          |  Apartments|     |  Office   |          \n"
+"          |____________|     |___________|      \n"
+"                                                  \n"
+"                  ##Gov District##           \n"
+
+ */
+ public class EastVillage {
+
+ }
---
+++ GovDistrict.java
@@ -0,0 +1,38 @@
+//gov district map
+
+//cops
+//fire fighter
+//hospital
+//city hall
+//court house
+//post office
+
+/*
+"                       ##School District##        \n"
+"          ______________     _______ \n"      ________
+"          |            |     |      |        |        |      \n"
+"          |  Court     |  |  |      \______  | Park   |          \n"
+"          |  House     |     |  Police     | |        |      \n"
+"		   |____________|||||||_____________| |__|  |__|			"
+"                       =     =
+"##Midtown  --  --  --  =     = --  --  -- -- -- -- -- --           ##High Way## "
+ "          ___________ =     =_____       _____________      \n"+ 
+"          |            |||||||     |___  |             |  \n"
+"          /   City     |     | Post    | | Fire        | \n"
+"          \   Hall     |  |  | Office  | |  Department |      \n"
+"          |___________ |     |_________| |_____________|               \n"
+"                                                  \n"
+"                  ##Gov District##           \n"
+
+
+
+
+
+
+
+
+*/
+
+public class GovDistrict {
+
+}
