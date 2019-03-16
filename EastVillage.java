b2ba077c0a0eccb12e981c341d64870757d3406b
Joel <joelgoodrum@hotmail.com>
2019-03-16 18:51:52 -0400

    SBeach and WBeach created

--- EastVillage.java
+++ EastVillage.java
@@ -1,20 +1,20 @@
 //east village map
 /*
-"                       ##School District##        \n"
+"                   #School District#        \n"
 "          ______________     _______________\n"
 "          |            |     |             | "
 "          | E.Village  |     | Senior      |               \n"
 "          |  Apartments|     |  Apartments |   "
 "		   |____________|||||||_____________|						"
 "                       =     =
-"##Midtown  --  --  --  =     = --  --  --            ##High Way## "
- "          ___________ =     =______          \n"+
+"#Midtown#  --  --  --  =     = --  --  --            #High Way## "
+"           ___________ =     =______          \n"+
 "          |            |||||||      |              \n"
 "          | May        |     | Post \____      \n"
 "          |  Apartments|     |  Office   |          \n"
 "          |____________|     |___________|      \n"
 "                                                  \n"
-"                  ##Gov District##           \n"
+"                   #Gov District#           \n"
 
  */
  public class EastVillage {
