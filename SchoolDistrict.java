559af4ecac82fc4d1d10d8f645ce6db56cab4642
Joel <joelgoodrum@hotmail.com>
2019-03-18 11:32:43 -0400

    add school district map

--- SchoolDistrict.java
+++ SchoolDistrict.java
@@ -19,15 +19,39 @@
  "           | Bath   |_   |  |  Lab     |      \n"+
  "           |  Rooms   |     |__________|      \n"+
  "           |__________|                                   \n"+
- "                                          ";
- "                ##E.Village##             "
+ "                                          \n"+
+ "                ##E.Village##             ";
 */
 
 
 public class SchoolDistrict{
 
+	//map
+	private static String map =  
+ "          _______________________________						\n"+
+ "                      =     =                                     \n"+
+ "##Uptown  --  --  --  =     = --  --  --            ##High Castles## \n"+
+ "          ___________ =     =____________  \n"+
+ "           ___________ ||||| ____________  \n"+
+ "          |           |  |  |            | \n"+
+ "          | Dorms     |     |            |       \n"+
+ "          |___________|  |  |  Physics   |       \n"+
+ "          /           |     |   & Tech   \\__      \n"+
+ "         |            |     | University    |     \n"+
+ "         |  Stadium   |  |  |               |  \n"+    
+ "         |            |     |_____ | _______|        \n"+  
+ "          \\          _|  |   __________                 \n"+    
+ "           \\________|       |  P&TU    |      \n"+
+ "           | Bath   |_   |  |  Lab     |      \n"+
+ "           |  Rooms   |     |__________|      \n"+
+ "           |__________|                                   \n"+
+ "                                          \n"+
+ "                ##E.Village##             ";
+
 	//school district level
 	public static void sDistrict(Player player){
+		//display map
+		System.out.println(map);
 		System.out.println("you're in the school district");
 	}
 }
